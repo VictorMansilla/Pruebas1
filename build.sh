1c7417fd14459e16c699b4f90b6f709229b4c35a
Victor Mansilla <victormansilla.vrm@gmail.com>
2025-02-11 20:01:06 -0300

    Septimo commit

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 set -o errexit
 
-# Actualizar pip
-pip install --upgrade pip setuptools
-
-# Instalar dependencias
 pip install -r requirements.txt
 
-# Aplicar migraciones
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+python manage.py makemigrations Usuarios
+
+python manage.py makemigrations Clientes
+
+python manage.py makemigrations Productos
+
+python manage.py migrate
